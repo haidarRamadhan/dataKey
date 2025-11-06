123f6138922ae97e9a2bb0f24089bd8a318d0106
Jeremia Julianto <juliantojeremia4@gmail.com>
2025-11-06 09:32:07 +0700

    update

--- api/src/app/api/rumah/route.ts
+++ api/src/app/api/rumah/route.ts
@@ -8,12 +8,27 @@ const prisma = new PrismaClient();
 
 // arrow function
 export const GET = async () => {
-    const data = await prisma.rumah.findMany({
-        orderBy: {
-            id: 'desc'
-        }
+  const data = await prisma.rumah.findMany({
+    orderBy: { id: "desc" },
+  });
+
+  return NextResponse.json(
+    { barang: data },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+      },
+    }
+  );
+};
+
+// handle preflight
+export const OPTIONS = async () =>
+    NextResponse.json({}, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
     });
-    return NextResponse.json({ 
-        barang: data
-     });
-}
