1f95a49614e6b20b7505c9ba64c3e7953dd2b2dc
Haidar Ramadhan <haidar_ramadhan_@teknokrat.ac.id>
2025-11-07 13:24:27 +0700

    Declare a logic to POST

--- api/src/app/api/rumah/route.ts
+++ api/src/app/api/rumah/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 // Create variable prisma client 
 const prisma = new PrismaClient();
-
+ 
 // arrow function
 export const GET = async () => {
   const data = await prisma.rumah.findMany({
@@ -13,7 +13,7 @@ export const GET = async () => {
   });
 
   return NextResponse.json(
-    { barang: data },
+    { Harga_rumah: data },
     {
       headers: {
         "Access-Control-Allow-Origin": "*",
@@ -32,3 +32,42 @@ export const OPTIONS = async () =>
           "Access-Control-Allow-Headers": "Content-Type",
         },
     });
+
+// create POST service (save data)
+export const POST = async (request: Request) => {
+    // read data from reqeust 
+    // then change into json
+    const data = await request.json();
+
+    // check if data has saved to avoid redudancty data
+    const checkData = await prisma.rumah.findFirst({
+      where: {
+        houseSize: data.houseSize,
+      },
+      select: {
+        houseSize: true
+      }
+    });
+
+    // if data code is exist 
+    if (checkData) {
+      return NextResponse.json({
+        message: "Ukuran rumah sudah ada",
+        success: false
+      })
+    }
+
+    // if data code doesn't exist
+    else {
+      // save houseSize as request
+      await prisma.rumah.create({
+        data: {
+          houseSize: data.houseSize,
+        }
+      });
+      return NextResponse.json({
+        message: "Data ukuran rumah berhasil disimpan",
+        success: true
+      })
+      }
+}
