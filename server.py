8e59e5c3374746cc9e49120405f63740a3d2f1c3
Haidar Ramadhan <haidar_ramadhan_@teknokrat.ac.id>
2025-12-13 08:11:34 +0700

    Modified

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ model = joblib.load("model.joblib")
 
 
 @app.post("/predict")
-def predit(data: dict):
+def predict(data: dict):
     # Expecting: { "houseSize": 120, "bedrooms": 3, "bathrooms": 2, "landSize": 180 }
     x = [[data["houseSize"], data["bedrooms"], data["bathrooms"], data["landSize"]]]
 
     pred = model.predict(x)[0]
-    return {"Prediction": pred}
+    return {"Prediction": float(pred)}
